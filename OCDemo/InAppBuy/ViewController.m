eac01dfe99ae1864560452dfdc1eebcc375441c5
shenzhenshihua <liangz@szmn.com.cn>
2018-03-19 11:53:59 +0800

    add

--- OCDemo/InAppBuy/ViewController.m
+++ OCDemo/InAppBuy/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 #import <StoreKit/StoreKit.h>
 #define SandboxUrl        @"https://sandbox.itunes.apple.com/verifyReceipt"//测试
+#define AppPurchaseUrl    @"https://buy.itunes.apple.com/verifyReceipt" //正式
+
 #define ThreeMonthsOfPurchase  @"yzzk.sub_3m" //购买三个月 yzzk_23_41
 //#define ThreeMonthsOfPurchase  @"yzzk_23_41" //单期购买
 @interface ViewController ()<SKProductsRequestDelegate,SKPaymentTransactionObserver>
@@ -132,6 +134,10 @@
  21007 收据信息是测试用（sandbox），但却被发送到产品环境中验证
  21008 收据信息是产品环境中使用，但却被发送到测试环境中验证
  
+ //验证需要注意点
+ 为保证审核的通过，需要在客户端或server进行双重验证，即，先以线上交易验证地址进行验证，如果苹果正式验证服务器的返回验证码code为21007，则再一次连接沙盒测试服务器进行验证即可。在应用提审时，苹果IAP提审验证时是在沙盒环境的进行的，即：苹果在审核App时，只会在sandbox环境购买，其产生的购买凭证，也只能连接苹果的测试验证服务器，如果没有做双验证，需要特别注意此问题，否则会被拒。
+  status = 21007
+ 校验成功 不会有status码
  */
 
 #pragma mark =====校验购买结果=====
@@ -144,43 +150,62 @@
             NSLog(@"receipt 本地数据不存在");
             return;
         }
+        NSDictionary * dict = [NSJSONSerialization JSONObjectWithData:receipt options:NSJSONReadingAllowFragments error:nil];
+        NSLog(@"原始数据==%@",dict);
+        //App 专用共享密钥  如果没有这个数据校验是错误 21004
+        //如何获得？ itunes content->功能->App内购买项目->App专用共享密钥
+        NSString * theString = @"7bd3e766df954e2f9532b35a3a03fd3e";
+        //编码 base64
         NSString *base64_receipt = [receipt base64EncodedStringWithOptions:NSDataBase64EncodingEndLineWithLineFeed];
-        
         NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
         [params setObject:base64_receipt forKey:@"receipt-data"];
-//        [params setObject:base64_receipt forKey:@"receipt"];
-
-        [params setObject:transaction.transactionIdentifier forKey:@"transaction_id"];
+        [params setObject:theString forKey:@"password"];
         
-        [self httpRequesWithUrl:SandboxUrl postData:params];
+        [self httpRequesWithUrl:AppPurchaseUrl postData:params completion:^(NSDictionary *result) {
+            if (result[@"status"]) {
+                if ([result[@"status"] floatValue] == 21007) {
+                    //是沙盒测试版本
+                    [self handleVerifySandbox:params];
+                }
+            } else {
+                //验证购买成功
+            }
+        }];
         
     }
-
-    
 }
 
-////编码 base64
+- (void)handleVerifySandbox:(NSDictionary *)params {
+    [self httpRequesWithUrl:SandboxUrl postData:params completion:^(NSDictionary *result) {
+        NSLog(@"%@",result);
+        if (!result[@"status"]) {
+            NSLog(@"验证成功购买过！！！");
+        }
+    }];
+}
+
+//////编码 base64
 //- (NSString*)base64String:(NSData *)parameterData{
 //    const uint8_t* input = (const uint8_t*)[parameterData bytes];
 //    NSInteger length = [parameterData length];
-//    
+//
 //    static char table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-//    
+//
 //    NSMutableData* data = [NSMutableData dataWithLength:((length + 2) / 3) * 4];
 //    uint8_t* output = (uint8_t*)data.mutableBytes;
-//    
+//
 //    NSInteger i;
 //    for (i=0; i < length; i += 3) {
 //        NSInteger value = 0;
 //        NSInteger j;
 //        for (j = i; j < (i + 3); j++) {
 //            value <<= 8;
-//            
+//
 //            if (j < length) {
 //                value |= (0xFF & input[j]);
 //            }
 //        }
-//        
+//
 //        NSInteger theIndex = (i / 3) * 4;
 //        output[theIndex + 0] =                    table[(value >> 18) & 0x3F];
 //        output[theIndex + 1] =                    table[(value >> 12) & 0x3F];
@@ -191,7 +216,7 @@
 //}
 
 //网络请求
-- (void)httpRequesWithUrl:(NSString *)urlString postData:(NSDictionary *)postData {
+- (void)httpRequesWithUrl:(NSString *)urlString postData:(NSDictionary *)postData completion:(void(^)(NSDictionary *result))completion {
     
     NSURLSession * session = [NSURLSession sharedSession];
     NSURL * url = [NSURL URLWithString:urlString];
@@ -205,7 +230,11 @@
     NSURLSessionTask * task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         if (!error) {
             NSDictionary * dict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
+//            environment = sandbox 代表是沙盒测试。
             NSLog(@"dict=%@",dict);
+            if (completion) {
+                completion(dict);
+            }
         }
         NSLog(@"response=%@",response);
     }];
